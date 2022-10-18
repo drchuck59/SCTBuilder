b5dc111ed8ce9c353b4ddfb2b1debe7d0c0162f9
Chuck Kowalewski <drchuck@hotmail.com>
2022-10-17 20:22:25 -0400

    VS updated resources

---
+++ dTTP_Reader/dTTP_Reader/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace dTTP_Reader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.stateComboBox = new System.Windows.Forms.ComboBox();
+            this.cityComboBox = new System.Windows.Forms.ComboBox();
+            this.RecordDataGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CycleLabel = new System.Windows.Forms.Label();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.AddedCheckBox = new System.Windows.Forms.CheckBox();
+            this.ChangedCheckBox = new System.Windows.Forms.CheckBox();
+            this.DeletedCheckBox = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.AirportDiagramsCheckBox = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.RecordDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // stateComboBox
+            // 
+            this.stateComboBox.FormattingEnabled = true;
+            this.stateComboBox.Location = new System.Drawing.Point(12, 67);
+            this.stateComboBox.Name = "stateComboBox";
+            this.stateComboBox.Size = new System.Drawing.Size(121, 23);
+            this.stateComboBox.TabIndex = 0;
+            this.stateComboBox.SelectedIndexChanged += new System.EventHandler(this.stateComboBox_SelectedIndexChanged);
+            // 
+            // cityComboBox
+            // 
+            this.cityComboBox.FormattingEnabled = true;
+            this.cityComboBox.Location = new System.Drawing.Point(155, 67);
+            this.cityComboBox.Name = "cityComboBox";
+            this.cityComboBox.Size = new System.Drawing.Size(121, 23);
+            this.cityComboBox.TabIndex = 1;
+            this.cityComboBox.SelectedIndexChanged += new System.EventHandler(this.cityComboBox_SelectedIndexChanged);
+            // 
+            // RecordDataGridView
+            // 
+            this.RecordDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.RecordDataGridView.Location = new System.Drawing.Point(12, 125);
+            this.RecordDataGridView.Name = "RecordDataGridView";
+            this.RecordDataGridView.RowTemplate.Height = 25;
+            this.RecordDataGridView.Size = new System.Drawing.Size(663, 292);
+            this.RecordDataGridView.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "State";
+            // 
+            // CycleLabel
+            // 
+            this.CycleLabel.AutoSize = true;
+            this.CycleLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CycleLabel.Location = new System.Drawing.Point(12, 9);
+            this.CycleLabel.Name = "CycleLabel";
+            this.CycleLabel.Size = new System.Drawing.Size(52, 21);
+            this.CycleLabel.TabIndex = 8;
+            this.CycleLabel.Text = "label2";
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(12, 96);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(75, 23);
+            this.SearchButton.TabIndex = 9;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(600, 96);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 10;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(155, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 15);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "City";
+            // 
+            // AddedCheckBox
+            // 
+            this.AddedCheckBox.AutoSize = true;
+            this.AddedCheckBox.Location = new System.Drawing.Point(293, 71);
+            this.AddedCheckBox.Name = "AddedCheckBox";
+            this.AddedCheckBox.Size = new System.Drawing.Size(61, 19);
+            this.AddedCheckBox.TabIndex = 12;
+            this.AddedCheckBox.Text = "Added";
+            this.AddedCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // ChangedCheckBox
+            // 
+            this.ChangedCheckBox.AutoSize = true;
+            this.ChangedCheckBox.Location = new System.Drawing.Point(360, 70);
+            this.ChangedCheckBox.Name = "ChangedCheckBox";
+            this.ChangedCheckBox.Size = new System.Drawing.Size(74, 19);
+            this.ChangedCheckBox.TabIndex = 13;
+            this.ChangedCheckBox.Text = "Changed";
+            this.ChangedCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // DeletedCheckBox
+            // 
+            this.DeletedCheckBox.AutoSize = true;
+            this.DeletedCheckBox.Location = new System.Drawing.Point(440, 71);
+            this.DeletedCheckBox.Name = "DeletedCheckBox";
+            this.DeletedCheckBox.Size = new System.Drawing.Size(66, 19);
+            this.DeletedCheckBox.TabIndex = 14;
+            this.DeletedCheckBox.Text = "Deleted";
+            this.DeletedCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(539, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 55);
+            this.button1.TabIndex = 15;
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // AirportDiagramsCheckBox
+            // 
+            this.AirportDiagramsCheckBox.AutoSize = true;
+            this.AirportDiagramsCheckBox.Location = new System.Drawing.Point(531, 71);
+            this.AirportDiagramsCheckBox.Name = "AirportDiagramsCheckBox";
+            this.AirportDiagramsCheckBox.Size = new System.Drawing.Size(144, 19);
+            this.AirportDiagramsCheckBox.TabIndex = 16;
+            this.AirportDiagramsCheckBox.Text = "Airport Diagrams Only";
+            this.AirportDiagramsCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 439);
+            this.Controls.Add(this.AirportDiagramsCheckBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DeletedCheckBox);
+            this.Controls.Add(this.ChangedCheckBox);
+            this.Controls.Add(this.AddedCheckBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.CycleLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RecordDataGridView);
+            this.Controls.Add(this.cityComboBox);
+            this.Controls.Add(this.stateComboBox);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "dTTP Reader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.RecordDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ComboBox stateComboBox;
+        private ComboBox cityComboBox;
+        private DataGridView RecordDataGridView;
+        private Label label1;
+        private Label CycleLabel;
+        private Button SearchButton;
+        private Button ExitButton;
+        private Label label2;
+        private CheckBox AddedCheckBox;
+        private CheckBox ChangedCheckBox;
+        private CheckBox DeletedCheckBox;
+        private Button button1;
+        private CheckBox AirportDiagramsCheckBox;
+    }
+}
---
+++ dTTP_Reader/dTTP_Reader/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dTTP_Reader
+{
+    public partial class Form1 : Form
+    {
+        public static DataSet dTTP = new dTTPDataSet();
+        public static DataTable state = new dTTPDataSet.state_dTTPDataTable();
+        public static DataTable city = new dTTPDataSet.city_dTTPDataTable();
+        public static DataTable airport = new dTTPDataSet.airport_dTTPDataTable();
+        public static DataTable record = new dTTPDataSet.recordDataTable();
+        public static string AIRAC = string.Empty;
+        public static DateTime AIRAC_start;
+        public static DateTime AIRAC_end;
+        public static CultureInfo provider = CultureInfo.InvariantCulture;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ReadXML_dTTP();
+            PopulateComboBoxes();
+            CycleLabel.Text = "AIRAC: " + AIRAC + " (" + AIRAC_start.ToUniversalTime().ToString("dd/MMM/yyyy HHmmK") + " to " + AIRAC_end.ToUniversalTime().ToString("dd/MMM/yyyy HHmmK") + ")";
+        }
+
+        private static void ReadXML_dTTP()
+        {
+            string xmlFile = "D:\\dTTP\\d-TPP_Metafile.xml";
+            string? state_ID;
+            string? city_ID;
+            string? airport_ID;
+
+            XmlDocument doc = new();
+            doc.Load(xmlFile);
+
+            XmlNode dTTPheader = doc.DocumentElement.SelectSingleNode("/digital_tpp");
+            // Get cycle value
+            AIRAC = dTTPheader.Attributes["cycle"]?.InnerText;
+            // Get start of cycle
+            string temp = dTTPheader.Attributes["from_edate"]?.InnerText;
+            temp = (temp.Substring(6, temp.Length - 6) + " " + temp.Substring(0,5)).Trim();
+            AIRAC_start = DateTime.ParseExact(temp, "MM/dd/yy HHmmZ", provider);
+            // Get end of cycle
+            temp = dTTPheader.Attributes["to_edate"]?.InnerText;
+            temp = (temp.Substring(6, temp.Length - 6) + " " + temp.Substring(0, 5)).Trim();
+            AIRAC_end = DateTime.ParseExact(temp, "MM/dd/yy HHmmZ", provider);
+            
+            foreach (XmlElement node in dTTPheader.ChildNodes)
+            {
+                if (node.Name == "state_code")
+                {
+                    state_ID = node.Attributes["ID"]?.InnerText;
+                    DataRow drs = state.NewRow();
+                    drs[0] = state_ID;
+                    drs[1] = node.Attributes["state_fullname"]?.InnerText;
+                    state.Rows.Add(drs);
+                    foreach (XmlNode node2 in node.ChildNodes)
+                    {
+                        city_ID = node2.Attributes["ID"]?.InnerText;
+                        DataRow drc = city.NewRow();
+                        drc[0] = city_ID;
+                        drc[1] = node2.Attributes["volume"]?.InnerText;
+                        drc[2] = state_ID;
+                        city.Rows.Add(drc);
+                        foreach (XmlNode node3 in node2.ChildNodes)
+                        {
+                            airport_ID = node3.Attributes["ID"]?.InnerText;
+                            DataRow dra = airport.NewRow();
+                            dra[0] = airport_ID;
+                            dra[1] = node3.Attributes["military"]?.InnerText;
+                            dra[2] = node3.Attributes["apt_ident"]?.InnerText;
+                            dra[3] = node3.Attributes["icao_ident"]?.InnerText;
+                            dra[4] = node3.Attributes["alnum"]?.InnerText;
+                            dra[5] = city_ID;
+                            dra[6] = state_ID;
+                            airport.Rows.Add(dra);
+                            foreach (XmlNode node4 in node3.ChildNodes)
+                            {
+                                int i = 0;
+                                DataRow drr = record.NewRow();
+                                foreach (XmlNode recordNode in node4.ChildNodes)
+                                {
+                                    if (i == 16)
+                                    {
+                                        if (recordNode.InnerText.Length != 0)
+                                        {
+                                            drr[i] = ParseDate(recordNode.InnerText);
+                                        }
+                                    }
+                                    else
+                                        drr[i] = recordNode.InnerText;
+                                    i++;
+                                }
+                                drr[17] = airport_ID;
+                                drr[18] = city_ID;
+                                drr[19] = state_ID;
+                                record.Rows.Add(drr);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void PopulateComboBoxes()
+        {
+            PopulateStateComboBox();
+            PopulateCityComboBox();
+        }
+
+        private void PopulateStateComboBox()
+        {
+            if (state.Rows.Count != 0)
+            {
+                stateComboBox.DataSource = state.DefaultView;
+                stateComboBox.DisplayMember = "fullname";
+                stateComboBox.ValueMember = "ID";
+                stateComboBox.MaxDropDownItems = 6;
+                stateComboBox.SelectedIndex = 0;
+                PopulateCityComboBox();
+            }
+        }
+
+        private void PopulateCityComboBox()
+        {
+            List<string> list = new();
+            DataView cityList = city.DefaultView;
+            if ((stateComboBox.SelectedIndex != -1) && (state.Rows.Count != 0))
+            {
+                cityList.RowFilter = "state_FK = '" + stateComboBox.SelectedValue + "'";
+                for (int iCount = 0; iCount < cityList.Count; iCount++)
+                {
+                    var val = cityList[iCount][0].ToString();
+                    if ((val != null) && val.Length != 0)
+                        if (!list.Contains(val))
+                            list.Add(val);
+                }
+                cityComboBox.DataSource = list;
+                cityComboBox.DisplayMember = "ID";
+                cityComboBox.MaxDropDownItems = 6;
+                cityComboBox.SelectedIndex = -1;
+            }
+        }
+
+        private void stateComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            PopulateCityComboBox();
+        }
+
+        private void cityComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cityComboBox.SelectedIndex != -1)
+            {
+                List<string> list = new();
+                for (int iCount = 0; iCount < airport.Rows.Count; iCount++)
+                {
+                    var val = airport.Rows[iCount][0].ToString();
+                    if ((val != null) && val.Length != 0)
+                        if (!list.Contains(val))
+                            list.Add(val);
+                }
+                list.Distinct();
+                list.Sort();
+            }
+            else
+            {
+                DataView dvAirport = new(airport);
+                dvAirport.Sort = "ID";
+                if ((cityComboBox.SelectedIndex != -1))
+                {
+                    dvAirport.RowFilter = "city_FK = '" + cityComboBox.SelectedValue + "'";
+                }
+            }
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            PopulateRecord();
+        }
+
+        private static DateTime ParseDate(string date)
+        {
+            return DateTime.Parse(date);
+        }
+
+        private void PopulateRecord()
+        {
+            string filter; bool includeDate = false;
+            DataView dvRecord = new(record);
+            dvRecord.Sort = "airport_FK";
+            if (cityComboBox.SelectedIndex != -1)
+                filter = "([city_FK] = '" + cityComboBox.SelectedValue + "')";
+            else
+                filter = "([state_FK] = '" + stateComboBox.SelectedValue + "')";
+            if (AddedCheckBox.Checked)
+            {
+                filter += " AND ([useraction] = 'A')";
+                includeDate = true;
+            }
+            if (ChangedCheckBox.Checked)
+            {
+                filter += " AND ([useraction] <> 'C')";
+                includeDate &= true;
+            }
+            if (DeletedCheckBox.Checked)
+            {
+                filter += " AND ([useraction] <> 'D')";
+                includeDate &= true;
+            }
+            if (AirportDiagramsCheckBox.Checked)
+            {
+                filter += " AND ([chart_code] = 'APD')";
+            }
+            if (filter.Length != 0)
+                dvRecord.RowFilter = filter;
+            BuildRecordDGV(dvRecord);
+        }
+
+        private void BuildRecordDGV(DataView dv)
+        {
+            DataTable dT = dv.ToTable(true, "airport_FK", "city_FK", "chart_code", "chart_name", "useraction", "pdf_name");
+            if (dT.Rows.Count > 0)
+            {
+                RecordDataGridView.DataSource = dT;
+                RecordDataGridView.Columns[0].HeaderText = "Airport";
+                RecordDataGridView.Columns[1].HeaderText = "City";
+                RecordDataGridView.Columns[2].HeaderText = "Chart";
+                RecordDataGridView.Columns[3].HeaderText = "Name";
+                RecordDataGridView.Columns[4].HeaderText = "Action";
+                RecordDataGridView.Columns[5].HeaderText = "PDF";
+                RecordDataGridView.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.DisplayedCells);
+            }
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+            // Calls the default browser to load the desired URL
+            // Note must create a new process instance and must trap for a computer with no browser
+        {
+            string target = "https://www.faa.gov/air_traffic/flight_info/aeronav/digital_products/dtpp/search/advanced/";
+            var psi = new System.Diagnostics.ProcessStartInfo();
+            psi.UseShellExecute = true;
+            psi.FileName = target;
+            try
+            {
+                Process.Start(psi);
+            }
+            catch (System.ComponentModel.Win32Exception noBrowser)
+            {
+                if (noBrowser.ErrorCode == -2147467259)
+                    MessageBox.Show(noBrowser.Message);
+            }
+            catch (System.Exception other)
+            {
+                MessageBox.Show(other.Message);
+            }
+        }
+    }
+}
